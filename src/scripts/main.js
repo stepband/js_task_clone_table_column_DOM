5670fb662911f4302d647a1c2f0953a377c4130f
stepband <vladshvik@gmail.com>
2023-04-07 17:12:06 +0300

    task completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,22 @@
 'use strict';
 
-// write your code here
+const headerBody = document.querySelector('thead').querySelectorAll('tr');
+
+const tableBody = document.querySelector('tbody').querySelectorAll('tr');
+
+const tableFoot = document.querySelector('tfoot').querySelectorAll('tr');
+
+function insertNode(nodeList) {
+  for (const nodeTr of nodeList) {
+    const rowList = nodeTr.querySelectorAll('td').length > 0
+      ? nodeTr.querySelectorAll('td')
+      : nodeTr.querySelectorAll('th');
+
+    rowList[0].parentNode.insertBefore(rowList[1]
+      .cloneNode(true), rowList[rowList.length - 1]);
+  }
+}
+
+insertNode(tableBody);
+insertNode(headerBody);
+insertNode(tableFoot);
